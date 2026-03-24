43e5fdd2b490e03f6e78b756757bc7f734097141
Fire_Fox <Danila.Shesterikov28@gmail.com>
2026-03-24 14:38:05 +0300

    Add files via upload

--- java/Main.java
+++ java/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         Garden produc = new Garden(" ПК", 19.6);
         Garden produc2 = new Garden(" монитор ", 17.6);
+        Garden produc3 = new Garden(" мышка ", 13.6);
 
         System.out.println("товар: ");
         produc.printInfo();
